5d76b91e1ff8f3d0a4adc47ee8d316e3add58057
Moody Rahman <mrahman01@stuy.edu>
2020-05-23 17:29:28 -0400

    jq test

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,26 +1,2 @@
-#!/usr/bin/env bash
 
-minecraft_dir="/home/moody/Desktop/server"
-
-echo $minecraft_dir
-
-cd $minecraft_dir
-ls
-
-# lsof -i:25565
-
-lsof -i:25565
-
-status=$?
-
-echo "THE RETURN CODE OF LSOF: $status"
-
-# code 0 = running server
-# cpde 1 = stopped server
-
-if [ "$status" == "0" ]
-then
-    cd $minecraft_dir
-else
-    echo "server already running!"
-fi
+cat config.json | jq ".jarname"
