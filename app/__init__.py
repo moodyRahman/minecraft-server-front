c72b74818ee964990bf72cb2c62edb5cb729c39a
Moody Rahman <mrahman01@stuy.edu>
2020-05-23 15:10:53 -0400

    execute server script and redir

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,28 @@ def index():
 	return render_template("index.html", server_status=server_state)
 
 
+@app.route("/cult/toggle")
+def turnonsever():
+	# a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+	# location = ("127.0.0.1", 80)
+
+	# result_of_check = a_socket.connect_ex(location)
+
+
+	# if result_of_check == 0:
+	# 	print("Port is open")
+	# 	p = Popen(["./scripts/start.sh", ""])
+	# 	p.wait()
+	# else:
+	# 	print("Port is not open")
+
+	p = Popen(["./scripts/start.sh", ""])
+	
+	return render_template("starting.html")
+
+
 
 if __name__ == "__main__":
 	app.run()
