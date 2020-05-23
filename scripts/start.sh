853cc00372851bd8e20a75397b571bb1518f04b0
Moody Rahman <mrahman01@stuy.edu>
2020-05-23 16:17:13 -0400

    timed script sleeps

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,9 +2,6 @@
 
 minecraft_dir="/home/moody/Desktop/server"
 
-echo $minecraft_dir
-
-cd $minecraft_dir
 ls
 
 # lsof -i:25565
@@ -21,8 +18,27 @@ echo "THE RETURN CODE OF LSOF: $status"
 if [ "$status" == "1" ]
 then
     screen -S minecraft -p 0 -X stuff "java -jar paper-307.jar nogui\n"
-    sleep 120m
+    echo "60" > stat.txt
+    sleep 15m
+    
+    screen -S minecraft -p 0 -X stuff "say 45 MINUTES LEFT IN THIS SESSION\n"
+    echo "45" > stat.txt
+    sleep 15m
+    
+    screen -S minecraft -p 0 -X stuff "say 30 MINUTES LEFT IN THIS SESSION\n"
+    echo "30" > stat.txt
+    sleep 15m
+    
+    screen -S minecraft -p 0 -X stuff "say 15 MINUTES LEFT IN THIS SESSION\n"
+    echo "15" > stat.txt
+    sleep 14m
+    
+    screen -S minecraft -p 0 -X stuff "say 1 MINUTE LEFT IN THIS SESSION\n"
+    echo "1" > stat.txt
+    sleep 1m
+    
     screen -S minecraft -p 0 -X stuff "stop\n"
+    echo "" > stat.txt
 else
     echo "server already running!"
 fi
