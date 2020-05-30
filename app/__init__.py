8eafba97e1e2ec211bb945ee8e56433db45c20f3
Moody Rahman <mrahman01@stuy.edu>
2020-05-30 16:04:33 -0400

    status API route

--- app/__init__.py
+++ app/__init__.py
@@ -8,34 +8,24 @@ app = Flask(__name__)
 
 @app.route("/cult")
 def index():
-	server_state = "java" in (p.name() for p in psutil.process_iter())
-	# time_left = open("stat.txt", "r").read()
+	return render_template("index.html")
 
+
+@app.route("/cult/status")
+def status():
+	server_state = "java" in (p.name() for p in psutil.process_iter())
 	with open("stat.txt", "r") as f:
 		time_left = f.read()
 		pass
 
-	return render_template("index.html", server_status=server_state, time=time_left)
+	return {"server_state": server_state, "time_left":time_left}
 
 
 @app.route("/cult/toggle")
 def turnonsever():
-	# a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-	# location = ("127.0.0.1", 80)
-
-	# result_of_check = a_socket.connect_ex(location)
-
-
-	# if result_of_check == 0:
-	# 	print("Port is open")
-	# 	p = Popen(["./scripts/start.sh", ""])
-	# 	p.wait()
-	# else:
-	# 	print("Port is not open")
-
 	p = Popen(["./scripts/start.sh", ""])
+
+	print(p.pid)
 	
 	return render_template("starting.html")
 
