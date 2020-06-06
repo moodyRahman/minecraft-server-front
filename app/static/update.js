c1261c865d584903a5c345ba2e7422f49a3bbe01
Moody Rahman <mrahman01@stuy.edu>
2020-06-06 16:51:05 -0400

    background colors

--- app/static/update.js
+++ app/static/update.js
@@ -2,48 +2,47 @@ status_e = document.getElementById("status");
 timer_e = document.getElementById("timer");
 ignite_e = document.getElementById("ignite")
 
-var draw = function(data){
+var draw = function (data) {
 	console.log(data);
 
-	
-	if (data.server_state){
+
+	if (data.server_state) {
 		ignite_e.disabled = true;
 		status_e.innerHTML = "minecult is online!";
 		timer_e.setAttribute("style", "visibility:visible;");
 		timer_e.innerHTML = "time left: " + data.time_left;
+		document.body.style.background = "#98fb98";
 	}
 	else {
 		ignite_e.disabled = false;
 		status_e.innerHTML = "minecult is offline";
 		timer_e.setAttribute("style", "visibility:hidden;");
 		timer_e.innerHTML = data.time_left;
-		ignite_e.innerHTML = "activate server"
+		document.body.style.background = "#fa867e";
 	}
 	// status_e.innerHTML = data.server_state;
 }
 
-var update = function(){
+var update = function () {
 	$.get("cult/status", draw);
 }
 
 
 update();
 
-setInterval(function(){
+setInterval(function () {
 	update();
 }, 10 * 60 * 1000);
 
 
-var ignite = function(){
-	$.get("/cult/status", function(d){
-		if (!d.server_state){
-			$.get("cult/toggle");
-			draw(
-				{
-					server_state:true, 
-					time_left:60
-				}
-			);
-		}
+var ignite = function () {
+	$.get("/cult/status", function (d) {
+		$.get("cult/toggle");
+		draw(
+			{
+				server_state: true,
+				time_left: 60
+			}
+		);
 	});
 }
